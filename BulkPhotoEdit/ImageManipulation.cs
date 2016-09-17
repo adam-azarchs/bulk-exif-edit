7c4ff14b08f328e965f17cd521d8976a3a2bc290
Adam Azarchs <azarchs@lookfar.net>
2016-09-16 23:12:05 -0700

    Initial version.

---
+++ BulkPhotoEdit/ImageManipulation.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+
+namespace BulkPhotoEdit
+{
+    class ImageManipulation
+    {
+        private ImageCodecInfo jpegCodecInfo = GetJpegCodec();
+        private static ImageCodecInfo GetJpegCodec()
+        {
+            return ImageCodecInfo.GetImageEncoders()
+                .First(enc => enc.MimeType == "image/jpeg");
+        }
+
+        public DateTime ReadDate(PropertyItem prop)
+        {
+            string dateString = Encoding.ASCII.GetString(prop.Value);
+            return DateTime.ParseExact(dateString,
+                exifDateFormat, CultureInfo.InvariantCulture);
+        }
+
+        public void WriteDate(PropertyItem prop, DateTime date)
+        {
+            prop.Value = Encoding.ASCII.GetBytes(date.ToString(
+                exifDateFormat, CultureInfo.InvariantCulture));
+        }
+
+        const string exifDateFormat = "yyyy:MM:dd HH:mm:ss\0";
+        const int DateTakenID = 0x9003;
+        const int OrientationPropID = 0x0112;
+
+        public EncoderValue? AdjustImage(string filename, bool fixOrientation, TimeSpan shift)
+        {
+            string newFileName = null;
+            EncoderValue? transform = null;
+            using (Image image = Bitmap.FromFile(filename))
+            {
+                var orientationProp = image.GetPropertyItem(OrientationPropID);
+                if (fixOrientation)
+                {
+                    transform = Orientation(orientationProp);
+                }
+                if (transform.HasValue || shift.Duration() > TimeSpan.Zero)
+                {
+                    orientationProp.Value = BitConverter.GetBytes((Int16)1);
+                    image.SetPropertyItem(orientationProp);
+                    if (shift.Duration() > TimeSpan.Zero)
+                    {
+                        var dateTakeProp = image.GetPropertyItem(DateTakenID);
+                        WriteDate(dateTakeProp, ReadDate(dateTakeProp) + shift);
+                        image.SetPropertyItem(dateTakeProp);
+                    }
+                    newFileName = Path.GetTempFileName();
+                    if (transform.HasValue)
+                    {
+                        var encParams = new EncoderParameters(1);
+                        encParams.Param[0] = new EncoderParameter(
+                            System.Drawing.Imaging.Encoder.Transformation,
+                            (long)transform.Value);
+                        image.Save(newFileName, jpegCodecInfo, encParams);
+                    }
+                    else
+                    {
+                        image.Save(newFileName);
+                    }
+                }
+            }
+            if (newFileName != null)
+            {
+                File.Delete(filename);
+                File.Move(newFileName, filename);
+            }
+            return transform;
+        }
+
+        private EncoderValue? Orientation(PropertyItem prop)
+        {
+            switch (BitConverter.ToInt16(prop.Value, 0))
+            {
+                case 1:
+                    return null;
+                case 2:
+                    return EncoderValue.TransformFlipHorizontal;
+                case 3:
+                    return EncoderValue.TransformRotate180;
+                case 4:
+                    return EncoderValue.TransformFlipVertical;
+                case 6:
+                    return EncoderValue.TransformRotate90;
+                case 8:
+                    return EncoderValue.TransformRotate270;
+                default:
+                    throw new NotImplementedException("Can't rotate and flip...");
+            }
+        }
+    }
+}
---
+++ BulkPhotoEdit/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace BulkPhotoEdit
+{
+    class Program
+    {
+        class CmdLineOptions
+        {
+            [Option('o', "fix-orientation",
+                HelpText = "Rotate all of the images according to their orientation exif data.")]
+            public bool FixOrientation { get; set; }
+
+            [Option('s', "shift-time",
+                HelpText = "Adjust date taken exif data by the given amount of time.")]
+            public string ShiftTime { get; set; }
+
+            [ValueList(typeof(List<string>))]
+            public List<string> FileNames { get; set; }
+
+            [HelpOption]
+            public string GetUsage()
+            {
+                // this without using CommandLine.Text
+                //  or using HelpText.AutoBuild
+                var usage = new StringBuilder();
+                usage.AppendLine("Bulk photo edit");
+                usage.AppendLine("Performs bulk edits on sets of JPEG images.");
+                return usage.ToString();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var options = new CmdLineOptions();
+            if (CommandLine.Parser.Default.ParseArguments(args, options) &&
+                options.FileNames.Count > 0)
+            {
+                string[] filenames = ExpandWildcards(options.FileNames).ToArray();
+                TimeSpan shift = TimeSpan.Zero;
+                if (options.ShiftTime != null && options.ShiftTime.Length > 0)
+                {
+                    if (!TimeSpan.TryParse(options.ShiftTime, out shift))
+                    {
+                        Console.Error.WriteLine("Invalid timespan string {0}", options.ShiftTime);
+                    }
+                }
+                FixOrientation(filenames, options.FixOrientation, shift);
+            }
+            else
+            {
+                // Display the default usage information
+                Console.WriteLine(options.GetUsage());
+            }
+        }
+
+        private static IEnumerable<string> ExpandWildcards(List<string> list)
+        {
+            foreach (string glob in list)
+            {
+                if (Path.IsPathRooted(glob))
+                {
+                    foreach (string filename in Directory.EnumerateFiles(Path.GetPathRoot(glob), glob))
+                    {
+                        yield return filename;
+                    }
+                }
+                else
+                {
+                    foreach (string filename in Directory.EnumerateFiles(Directory.GetCurrentDirectory(), glob))
+                    {
+                        yield return filename;
+                    }
+                }
+            }
+        }
+
+        private static void FixOrientation(
+            IEnumerable<string> filenames, bool fixOrientation, TimeSpan shift)
+        {
+            ImageManipulation manip = new ImageManipulation();
+            foreach (string filename in filenames)
+            {
+                var rot = manip.AdjustImage(filename, fixOrientation, shift);
+                if (rot.HasValue)
+                {
+                    Console.WriteLine("Rotated {0} by {1}", filename, rot.Value);
+                }
+                else
+                {
+                    Console.WriteLine("Processed {0}", filename);
+                }
+            }
+        }
+    }
+}
